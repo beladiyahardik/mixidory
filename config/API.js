e10738479a57daa8c4a092d63fc2ee10df4018e7
Hardik Beladiya <72540473+beladiyahardik@users.noreply.github.com>
2022-04-04 09:22:11 +0530

    APIs ARE LIVE NOW

--- config/API.js
+++ config/API.js
@@ -1,4 +1,4 @@
-const BaseURL = "http://localhost/Mixidory-backend";
+const BaseURL = "https://mukhicorporation.com/mixidory";
 
 export const LOGIN_API = `${BaseURL}/users/login`;
 export const SIGNUP_API = `${BaseURL}/users/signup`;
